80c54d544d35a234c87923791116553d13f36b9d
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-02-02 16:05:45 +0100

    MAJ

---
+++ classes/Hotel.php
@@ -0,0 +1,72 @@
+<?php
+
+class Hotel{
+
+    private string $name;
+    private string $star;
+    private string $address;
+    private int $postalCode;
+    private string $town;
+    
+    
+    public function __construct(string $name, string $star, string $address,int $postalCode, string $town)
+    {
+        $this->name = $name;
+        $this->star = $star;
+        $this->address =$address;
+        $this->postalCode = $postalCode;
+        $this->town = $town;
+    }
+
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+  
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getStar(): string {
+        return $this->star;
+    }
+
+    public function setStar(string $star): self {
+        $this->star = $star;
+        return $this;
+    }
+
+    public function getAddress(): string {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function getPostalCode(): int {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(int $postalCode): self {
+        $this->postalCode = $postalCode;
+        return $this;
+    }
+
+    public function getTown(): string {
+        return $this->town;
+    }
+
+    public function setTown(string $town): self {
+        $this->town = $town;
+        return $this;
+    }
+
+
+    public function getInfo(){
+        return "<h3>".$this->name." ".$this->star." ".$this->town."</h3> ".$this->address." ".$this->postalCode." ". $this->town;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <h1>Exercice Hotel POO</h1>
 
 <?php
+
+spl_autoload_register(function($class){ //permet d'indiquer le chemin des classes utiliser.
+    require 'classes/'.$class.'.php';
+});
+
+$hilton = new Hotel("Hilton","****","10 route de la gare",67000,"Strasbourg");
+
+echo $hilton->getInfo();
